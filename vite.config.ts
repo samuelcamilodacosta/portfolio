14e958efdda6b04ca698eaef3039e7a64ef82abd
Samuel Costa <samuka.camilo@gmail.com>
2026-06-10 01:12:05 -0300

    feat: adicionando base do repositório

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 const rootDir = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
